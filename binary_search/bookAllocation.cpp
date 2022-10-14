c723497b5ca5e3d3ef31362ca9508c5500e4fd97
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2022-10-15 01:47:58 +0530

    Create bookAllocation.cpp

---
+++ binary_search/bookAllocation.cpp
@@ -0,0 +1,63 @@
+/* given no of student k
+  array containing the pages in the book at perticular index
+  we have to break the array into k parts
+  such that maximum of the array sum will be minimum
+  e.g. 10,20,30,40   if we have to break this into 3
+  then ans => 10,20 | 30 | 40
+*/
+
+// patil_gm
+#include<vector>
+using namespace std;
+
+bool isPossible(vector<int> arr, int n, int m, int mid) {
+    int studentCount = 1;
+    int pageSum = 0;
+    //cout << "checking for mid "<< mid <<endl;
+    
+    for(int i = 0; i<n; i++) {
+        if(pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else
+        {
+            studentCount++;
+            if(studentCount > m || arr[i] > mid ) {
+            return false;
+        }
+            pageSum = arr[i];
+        }
+        if(studentCount > m) {
+            return false;
+        }
+        //cout << " for i " << i << " Student "<< studentCount << " pageSum " << pageSum << endl;
+    }
+    return true;
+}
+
+int allocateBooks(vector<int> arr, int n, int m) {
+    int s = 0;
+    int sum = 0;
+    
+    for(int i = 0; i<n; i++) {
+        sum += arr[i];
+    }
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    
+    while(s<=e)
+    {
+        if(isPossible(arr,n,m,mid)) {
+            //cout<<" Mid returned TRUE" << endl;
+            ans = mid;
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
