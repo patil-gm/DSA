eeb600d62e070687469477b140f0503aa80c89d7
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2022-10-16 14:17:21 +0530

    selection sort
    The selection sort algorithm sorts an array by repeatedly finding the minimum element 
    (considering ascending order) from the unsorted part and putting it at the beginning. 
    
    The algorithm maintains two subarrays in a given array.
    
    The subarray which already sorted. 
    The remaining subarray was unsorted.
    In every iteration of the selection sort, the minimum element (considering ascending order) 
    from the unsorted subarray is picked and moved to the sorted subarray.

---
+++ sorting/selectionSort.cpp
@@ -0,0 +1,28 @@
+/*
+The selection sort algorithm sorts an array by repeatedly finding the minimum element 
+(considering ascending order) from the unsorted part and putting it at the beginning. 
+
+The algorithm maintains two subarrays in a given array.
+
+The subarray which already sorted. 
+The remaining subarray was unsorted.
+In every iteration of the selection sort, the minimum element (considering ascending order) 
+from the unsorted subarray is picked and moved to the sorted subarray. 
+
+*/
+
+
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i = 0; i < n-1; i++ ) {
+        int minIndex = i;
+        
+        for(int j = i+1; j<n; j++) {
+            
+            if(arr[j] < arr[minIndex]) 
+                minIndex = j;
+            
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
