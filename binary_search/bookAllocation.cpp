cab0af474f108a783ace33fe1eb9d3b0c8169fb3
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2022-10-16 13:56:44 +0530

    Update bookAllocation.cpp

--- binary_search/bookAllocation.cpp
+++ binary_search/bookAllocation.cpp
@@ -4,6 +4,14 @@
   such that maximum of the array sum will be minimum
   e.g. 10,20,30,40   if we have to break this into 3
   then ans => 10,20 | 30 | 40
+  
+You are given N number of books. Every ith book has Ai number of pages. The books are arranged in ascending order.
+You have to allocate contiguous books to M number of students. There can be many ways or permutations to do so. 
+In each permutation, one of the M students will be allocated the maximum number of pages. Out of all these permutations, 
+the task is to find that particular permutation in which the maximum number of pages allocated to a student 
+is the minimum of those in all the other permutations and print this minimum value.
+Each book will be allocated to exactly one student. Each student has to be allocated at least one book.
+Note: Return -1 if a valid assignment is not possible, and allotment should be in contiguous order (see the explanation for better understanding).
 */
 
 // patil_gm
@@ -39,6 +47,10 @@ bool isPossible(vector<int> arr, int n, int m, int mid) {
 }
 
 int allocateBooks(vector<int> arr, int n, int m) {
+    if(n<m)return -1;
+//     n=>no of books
+//     m=>no of students
+  
     int s = 0;
     int sum = 0;
     
