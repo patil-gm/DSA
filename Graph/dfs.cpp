cd1ae7922581cfb556e34b339d5a3d65411b1b96
Ghanashyam Patil <88094853+patil-gm@users.noreply.github.com>
2022-12-30 03:40:36 +0530

    Update dfs.cpp

--- Graph/dfs.cpp
+++ Graph/dfs.cpp
@@ -1,1 +1,41 @@
+// interative dfs
+void dfs (int node, unordered_map<int,bool> &visited, unordered_map<int,list<int>> &adj,vector<int> ans){
+
+    stack<int> stack;
+    stack.push(node);
+ 
+    while (!stack.empty())
+    {
+        int s = stack.top();
+        stack.pop();
+ 
+        if (!visited[s])
+        {
+            ans.push_back(node);
+            visited[s] = true;
+        }
+
+        for (auto it : adj[node]){
+            if (!visited[it]) stack.push(it);
+        }
+      
+    }
+  
+}
+  
+
+// recursive dfs
+void dfs (int node, unordered_map<int,bool> &visited, unordered_map<int,list<int>> &adj,vector<int> ans){
+  ans.push_back(node);
+  visited[node]=true;
+  
+// for every node make recusive call
+for(auto it:adj[node]){
+  if(!vidited[it]){
+    dfs(it,visited,adj,ans)
+   }
+}
+};
+
+
 
